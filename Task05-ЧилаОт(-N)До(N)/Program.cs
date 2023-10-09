b08ec1e93d5d4de96335f0ea14ca120793fb36e0
IlyaNovosad <IlyaNovosad@mail.ru>
2023-10-09 22:20:06 +0300

    Add a new project Task05

---
+++ Task05-ЧилаОт(-N)До(N)/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Напишите программу, которая на вход получает число N, а на выходе показывает вме числа в промежутке от -N до N.
+*/
+
+Console.Write("Введите с клавиатуры число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+for (int i = - userNumber1; i <= userNumber1; i++) {
+    Console.Write(i + " ");
+}
