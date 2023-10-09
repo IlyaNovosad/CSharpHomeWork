c212d33b0d4b9e8e3b240e8c71dd4f8a7ca40d04
IlyaNovosad <IlyaNovosad@mail.ru>
2023-10-09 23:10:49 +0300

    Add a new project task04

---
+++ Task04-НахождениеМаксимальногоЧисла/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Внутри класса Answer напишите метод FindMax, 
+который принимает на вход три числа и выдаёт максимальное из этих чисел.
+*/
+
+    System.Console.Write("Введите первое число: ");
+    int firstNumber = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Введите второе число: ");
+    int secondNumber = Convert.ToInt32(Console.ReadLine());
+    System.Console.Write("Введите третье число: ");
+    int thirdNumber = Convert.ToInt32(Console.ReadLine());
+    if (firstNumber > secondNumber && firstNumber > thirdNumber) {
+        int max = firstNumber;
+        Console.WriteLine("Максимальным из трех чисел является: " + max);
+        } if (secondNumber > firstNumber && secondNumber > thirdNumber) {
+            int max = secondNumber;
+            Console.WriteLine("Максимальным из трех чисел является: " + max);
+        } if (thirdNumber > firstNumber && thirdNumber > secondNumber) {
+                int max = thirdNumber;
+                Console.WriteLine("Максимальным из трех чисел является: " + max);
+            }
