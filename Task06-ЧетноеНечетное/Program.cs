bff4eba588ebb21cb56fe56ae258b65833c3f8e4
IlyaNovosad <IlyaNovosad@mail.ru>
2023-10-10 23:52:37 +0300

    Add a new project Task06

---
+++ Task06-ЧетноеНечетное/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Внутри класса Answer напишите метод CheckIfEven, 
+который на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+*/
+using System;
+System.Console.Write("Введите число для проверки: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int surPlus = number % 2;
+if (surPlus > 0) {
+    Console.WriteLine($"{number} нечетное!");
+} else {
+    Console.WriteLine($"{number} четное!");
+}
