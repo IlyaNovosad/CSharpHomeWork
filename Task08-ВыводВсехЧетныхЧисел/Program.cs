2d9d0f06b293bd7c1490320fc17af380319256c3
IlyaNovosad <IlyaNovosad@mail.ru>
2023-10-11 00:07:22 +0300

    Add a new project Task08

---
+++ Task08-ВыводВсехЧетныхЧисел/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿/* Внутри класса Answer напишите метод PrintEvenNumbers, 
+* которая на вход принимает число (number), 
+* а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+*/
+Console.Write("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number; i = i + 2) {
+    Console.Write(i + " ");
+}
