d6e02edcd5cb2af0c0ed9031584afcc49fd962bf
IlyaNovosad <IlyaNovosad@mail.ru>
2023-10-14 22:51:35 +0300

    Add a new project Task10

---
+++ Task11-УдаляетВторуюЦифруТрехзначногоЧисла/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int number = RandomNumber();
+int RandomNumber() {
+    int newRandom =new Random().Next(99, 999);
+    return newRandom;
+}
+int FirstPosition(int number) {
+    int firstPosition = number / 100;
+    return firstPosition;
+}
+int ThirdPosition(int nuber) {
+    int thirdPosition = nuber % 10;
+    return thirdPosition;
+}
+
+Console.WriteLine(number);
+Console.WriteLine(FirstPosition(number)*10 + ThirdPosition(number));
