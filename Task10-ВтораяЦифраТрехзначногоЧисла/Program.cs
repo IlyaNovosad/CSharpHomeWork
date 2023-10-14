c4b81bd72f3a9196e20271bdeeae07b3dab53b40
IlyaNovosad <IlyaNovosad@mail.ru>
2023-10-14 21:17:24 +0300

    Add a new project Task10

---
+++ Task10-ВтораяЦифраТрехзначногоЧисла/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondPosition (int nuber) {
+    if (number > 0 && number < 1000) {
+        int firstPosition = nuber / 10;
+        int secondPosition = firstPosition % 10;
+        return secondPosition;
+    } else {
+        Console.WriteLine($"Число {number} не трехзначное.");
+        return 0;
+    }
+}
+
+Console.WriteLine(SecondPosition(number));
