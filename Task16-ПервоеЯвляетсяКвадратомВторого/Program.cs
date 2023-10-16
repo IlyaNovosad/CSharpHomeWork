0e9226834aca3840511cb691e3592e6e07e459fd
IlyaNovosad <IlyaNovosad@mail.ru>
2023-10-16 23:46:15 +0300

    Do some changes in Task16

--- Task16-ПервоеЯвляетсяКвадратомВторого/Program.cs
+++ Task16-ПервоеЯвляетсяКвадратомВторого/Program.cs
@@ -12,7 +12,9 @@ Console.Write("Введите вторе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
 int Sqr(int arg1, int arg2) {
-    if (arg1 == arg2 * arg2) {
+    int sqrOne = arg1 * arg1;
+    int sqrTwo = arg2 * arg2;
+    if (arg1 == sqrTwo || arg2 == sqrOne) {
         Console.WriteLine($"Число {arg1} является квадратом числа {arg2}.");
         return 1;
     } else {
